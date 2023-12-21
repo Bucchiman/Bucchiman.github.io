12f74cc707a772538db3068ee71701c285352102
Yuki Iwabuchi <8ucchiman@gmail.com>
2023-12-21 17:48:29 +0900

    add for react apps

--- app.tsx
+++
@@ -1,9 +0,0 @@
-import { React } from "https://dev.jspm.io/react@17.0.2";
-import { render } from "https://dev.jspm.io/react-dom@17.0.2";
-
-const App = () => {
-    return <h1>Hello, Deno + React!</h1>;
-};
-
-
-render(<App />, document, getElementById("root"));
